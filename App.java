15a1608e2b6ab47ae9fc21c47c2498d3532ebc3b
TheLordCoder <diablala72@gmail.com>
2019-03-01 16:22:16 +0100

    added App.java

---
+++ App.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("orange");
+        list.add("banana");
+        list.add("strawbery");
+
+    }
+}
